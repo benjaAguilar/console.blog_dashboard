e777cfb11ae93e0f97195d15a66ec8934b0e376c
ColorCode <benjaaguilar02@gmail.com>
2025-03-10 15:33:59 -0300

    feat: add form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { useRef } from "react";
 import styles from "./css/App.module.css";
+import { fetchData } from "./utils/utils";
 
 function App() {
+  const formRef = useRef();
+
+  async function login(e) {
+    e.preventDefault();
+
+    const formData = new FormData(formRef.current);
+    const data = new URLSearchParams(formData);
+
+    const loginData = await fetchData(
+      "/api/users/login",
+      "POST",
+      { "Content-Type": "application/x-www-form-urlencoded" },
+      data
+    );
+
+    if (loginData.success && loginData.user.role === "ADMIN") {
+      window.location.href = "/dashboard";
+      return;
+    }
+
+    alert("Incorrect login");
+  }
+
   return (
     <>
       <h1>Log in</h1>
-      <form action="">
+      <form action="" ref={formRef}>
         <div className={styles.fieldBox}>
           <label htmlFor="username">Username</label>
           <input type="text" name="username" id="username" />
@@ -13,7 +38,12 @@ function App() {
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
         </div>
-        <input type="submit" />
+        <input
+          type="submit"
+          onClick={(e) => {
+            login(e);
+          }}
+        />
       </form>
     </>
   );
